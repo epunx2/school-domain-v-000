d4ba625ccfdf0dcb48f9e945c48d6d3b35e1096a
Eric Chrobak <eric@ericchrobak.com>
2018-05-17 01:52:01 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,12 +10,8 @@ class School
   end
 
   def add_student(student, grade)
-    if roster[grade] == ""
       roster[grade] = []
       roster[grade] << student
-    else
-      roster[grade] << student
-    end
   end
 
   def grade
