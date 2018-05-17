70d1c2a8f790aa91ad7ddf03aa499b22498713db
Eric Chrobak <eric@ericchrobak.com>
2018-05-17 01:58:41 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,8 +10,12 @@ class School
   end
 
   def add_student(student, grade)
-    @roster[grade] = []
-    @roster[grade] << student
+    if @roster.has_key?(grade)
+      @roster[grade] << student
+    else
+      @roster[grade] = []
+      @roster[grade] << student
+    end
   end
 
   def grade
