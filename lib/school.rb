1a16875006395b6a9adce86e76d192f8671e46fe
Eric Chrobak <eric@ericchrobak.com>
2018-05-17 02:03:49 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -3,15 +3,11 @@ class School
 
   attr_accessor :school, :roster
 
-  def roster
-    @roster = Hash.new(false)
-  end
-
   def add_student(student, grade)
 
       @roster[grade] = []
       @roster[grade] << student
-    
+
   end
 
   def grade
